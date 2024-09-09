7df89cf413779082278660742d0ef4c175f4a5fb
pawan2402 <pawanshekhawat2402@gmail.com>
2024-09-09 05:42:08 +0530

    Importing Typo Fix

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Header from "../components/Header";
 import CategoryMenu from "../components/CategoryMenu";
 import TopAttendee from "../components/TopAttendee";
-import Banner from "../components/banner";
+import Banner from "../components/Banner";
 
 const Home = () => {
   return (
